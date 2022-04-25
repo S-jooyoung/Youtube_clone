292d67d286464f3502d8b2f132470f2dbb92b591
S-jooyoung <jooyoung.dev@gmail.com>
2022-04-25 20:00:00 +0000

    Feat: search_header 스냅샷 테스트

--- src/components/search_header/tests/search_header.test.js
+++ src/components/search_header/tests/search_header.test.js
@@ -4,25 +4,38 @@ import React from "react";
 // import react-testing methods
 import { render, screen } from "@testing-library/react";
 
+// import custom-component
 import SearchHeader from "../search_header";
 import userEvent from "@testing-library/user-event";
 
+// import snapshot-testing methods
+import renderer from "react-test-renderer";
+
 describe("SearchHeader", () => {
-  let onSearch;
-  let input;
-  let button;
+  it("renders", () => {
+    const component = renderer
+      .create(<SearchHeader onSearch={jest.fn()} />)
+      .toJSON();
 
-  beforeEach(() => {
-    onSearch = jest.fn();
-    render(<SearchHeader onSearch={onSearch} />);
-    input = screen.getByPlaceholderText("Search...");
-    button = screen.getByTestId("test-button");
+    expect(component).toMatchSnapshot();
   });
+  describe("Input Sumit", () => {
+    let onSearch;
+    let input;
+    let button;
 
-  it("calls onSearch when button clicked", () => {
-    userEvent.type(input, "BTS");
-    userEvent.click(button);
+    beforeEach(() => {
+      onSearch = jest.fn();
+      render(<SearchHeader onSearch={onSearch} />);
+      input = screen.getByPlaceholderText("Search...");
+      button = screen.getByTestId("test-button");
+    });
 
-    expect(onSearch).toHaveBeenCalledWith("BTS");
+    it("calls onSearch when button clicked", () => {
+      userEvent.type(input, "BTS");
+      userEvent.click(button);
+
+      expect(onSearch).toHaveBeenCalledWith("BTS");
+    });
   });
 });
